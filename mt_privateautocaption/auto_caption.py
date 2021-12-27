0a6439d9e8e7cd76b6f88bbc99939c5f7d9e0f38
botac135 <86068776+botac135@users.noreply.github.com>
2021-12-27 16:17:01 +0530

    Update auto_caption.py

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -16,8 +16,7 @@ URL_LINK=Config.URL_LINK
 @Client.on_message(filters.media & filters.channel)
 async def caption(client, message: Message):
     kopp, _ = get_file_id(message)
-    await message.edit(f"`{kopp.file_name}`\n<i><b>{CAPTION_TEXT}</b></i>\n\n🎥𝙛𝙧𝙚𝙖𝙠𝙚𝙧𝙯🧐𝙛𝙞𝙡𝙢𝙮™🍿©
-▬▬▬▬▬▬▬▬▬▬▬▬",
+    await message.edit(f"`{kopp.file_name}`\n<i><b>{CAPTION_TEXT}</b></i>\n\n🎥𝙛𝙧𝙚𝙖𝙠𝙚𝙧𝙯🧐𝙛𝙞𝙡𝙢𝙮™🍿©",
           reply_markup=InlineKeyboardMarkup(
               [[
               InlineKeyboardButton(f"{BUTTON_TEXT}", url=f"{URL_LINK}")
